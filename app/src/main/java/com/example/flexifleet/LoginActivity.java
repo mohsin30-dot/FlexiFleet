3d7e82f3e51553d3b67b72b2a04b86454ac712f3
Mohsin Rubel <zemimamuskan@mohsins-Imac.local>
2020-07-04 03:39:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/flexifleet/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.flexifleet;
+
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
+
+
+
