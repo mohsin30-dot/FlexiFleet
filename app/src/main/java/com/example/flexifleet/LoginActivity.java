5851eda6a67e474a76550b3b85fce64cd72f3973
zemimamuskan <msis.rubel@gmail.com>
2020-07-07 01:21:08 -0400

    -OrderFood Activity added

--- app/src/main/java/com/example/flexifleet/LoginActivity.java
+++ app/src/main/java/com/example/flexifleet/LoginActivity.java
@@ -4,13 +4,31 @@ package com.example.flexifleet;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginBT;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        loginBT = findViewById(R.id.loginButton);
+
+        loginBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,HomeScreen.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
 
